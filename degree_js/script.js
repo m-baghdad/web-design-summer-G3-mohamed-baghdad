1b87e0929bc59a6c1dfb7cbb53c72fb9bd8f1f74
m-baghdad <mohamedbaghdad885@gmail.com>
2026-07-19 22:20:36 +0300

    Add files via upload

---
+++ degree_js/script.js
@@ -0,0 +1,13 @@
+var degree = prompt("Enter your degree: ")
+if (degree >= 90){
+    console.log("A")
+}
+else if(degree >= 80){
+    console.log("B")
+}
+else if(degree >= 70){
+    console.log("C")
+}
+else{
+    console.log("F")
+}
